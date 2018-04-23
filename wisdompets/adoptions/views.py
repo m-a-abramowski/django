80cbc3b68cae6b2190af1ceddf6f34a6d68c6364
Maciek <m.a.abramowski@gmail.com>
2018-04-23 15:58:02 +0200

    routes

--- wisdompets/adoptions/views.py
+++ wisdompets/adoptions/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('<p>home view</p>')
+
+def pet_detail(request, id):
+    return HttpResponse('<p>pet_detail view wit the id {}</p>'.format(id))
 
 # Create your views here.
